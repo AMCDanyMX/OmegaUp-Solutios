c1313700831d50847902e819b388965c6dc1b56a
Daniel Castillo <60952744+AMCDanyMX@users.noreply.github.com>
2022-01-26 20:46:11 -0600

    Create positivo_negativo_cero.c
    3292. DFD-Positivo Negativo o Cero Este problema ha sido reconocido como un Problema de Calidad por un comité de jueces de omegaUp

---
+++ positivo_negativo_cero.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+    int n;
+
+    scanf("%d",&n);
+    
+    if(n<0){
+    printf("NEGATIVO");
+    }
+    else if(n>0){
+        printf("POSITIVO");
+    }
+    else if(n==0){
+        printf("CERO");
+    }
+  
+  
+  
+  
