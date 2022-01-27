57b9a7fd20bc9aed6e6aace8ced645c09d9f5f8f
Daniel Castillo <60952744+AMCDanyMX@users.noreply.github.com>
2022-01-26 20:45:06 -0600

    Create volumen_casa.c
    1993. Volumen de la Casa Este problema ha sido reconocido como un Problema de Calidad por un comité de jueces de omegaUp

---
+++ volumen_casa.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int ancho,largo,alto,total;
+
+    scanf("%d %d %d",&ancho,&largo,&alto);
+
+    total = ancho*largo*alto;
+
+    printf("%d",total);
+
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+}
