29f4c7c000299e7816f1d4ecb06c1bc8ecdda6ca
Daniel Castillo <60952744+AMCDanyMX@users.noreply.github.com>
2022-01-26 20:38:39 -0600

    Create bienvenido_cmasmas.cpp
    9345. B01 Bienvenido a C++ II Este problema ha sido reconocido como un Problema de Calidad por un comité de jueces de omegaUp

---
+++ bienvenido_cmasmas.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout<<"Bienvenido a C++\n";
+    }
+    
+
+
+
+    return 0;
+}
