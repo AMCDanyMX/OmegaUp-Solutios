89a2ce1a02a429f7cdb9fccacaba3a4ae64c31a0
Daniel Castillo <60952744+AMCDanyMX@users.noreply.github.com>
2022-01-26 20:43:59 -0600

    Create multiplicaciones.c
    4. multiplicaciones Este problema ha sido reconocido como un Problema de Calidad por un comité de jueces de omegaUp

---
+++ multiplicaciones.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,prod,prod1,prod2;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    prod1 = (a*1);
+    prod2 =(1*b);
+
+    prod = prod1+prod2;
+
+
+
+    printf("%d",prod);
+
+    return 0;
+}
