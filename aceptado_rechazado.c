a6aa2bc90b63c2ca7179c000c0bf9717d628049f
Daniel Castillo <60952744+AMCDanyMX@users.noreply.github.com>
2022-01-26 20:39:18 -0600

    Create aceptado_rechazado.c
    5712. DFD-Aceptado Rechazado

---
+++ aceptado_rechazado.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    float est;
+
+    scanf("%f",&est);
+
+    if(est>1.70){
+        printf("Aceptado");
+    }else{
+        printf("Rechazado");
+        
+    }
+
